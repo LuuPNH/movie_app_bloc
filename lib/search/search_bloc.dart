ffe1cee5f1d64fc26c62296bac8554b7a1455fd1
hailuu.1999@gamil.com <Hailuu123@>
2021-10-25 09:17:41 +0700

    Fix xong state.page

--- lib/search/search_bloc.dart
+++ lib/search/search_bloc.dart
@@ -5,35 +5,31 @@ import 'package:movie_app_bloc/search/search_state.dart';
 import 'package:teq_flutter_core/teq_flutter_core.dart';
 
 class SearchMovieBloc extends BaseBloc<SearchMovieState> {
-  SearchMovieBloc() : super(SearchMovieState(isLoading: true, errorRefresh: false));
+  SearchMovieBloc()
+      : super(SearchMovieState(isLoading: true, errorRefresh: false));
 
   MovieRepository movieRepository = MovieRepository();
-  int pageKey = 1;
-
   @override
   Stream<SearchMovieState> mapEventToState(BaseEvent event) async* {
     if (event is SearchMovieEvent) {
-      pageKey = 1;
-      var list =
-          await movieRepository.getSearchMovies(event.name, pageKey);
-      if(list.isEmpty){
-        yield state.copyWith(errorRefresh: true, page: pageKey);
+      var list = await movieRepository.getSearchMovies(event.name, state.page);
+      if (list.isEmpty) {
+        yield state.copyWith(errorRefresh: true, page: state.page + 1);
       }
-      yield state.copyWith(list: list,errorRefresh: false);
+      yield state.copyWith(list: list, errorRefresh: false);
     } else if (event is SearchMovieMoreEvent) {
       List<Movie>? li;
       if (state.list?.isNotEmpty == true) {
-        pageKey += 1;
         List<Movie>? _list =
-            await movieRepository.getSearchMovies(event.name, pageKey);
-        if(_list.isEmpty == true){
-          yield state.copyWith(errorLoadmore: true, page: pageKey);
+            await movieRepository.getSearchMovies(event.name, state.page);
+        if (_list.isEmpty == true) {
+          yield state.copyWith(errorLoadmore: true, page: state.page + 1);
         }
         li = [...state.list!, ..._list];
         yield state.copyWith(list: li);
       }
     } else if (event is SearchMovieStartEvent) {
-      yield state.copyWith(isFirstLoad: true,page: pageKey);
+      yield state.copyWith(isFirstLoad: true, page: state.page + 1);
     }
   }
 }
